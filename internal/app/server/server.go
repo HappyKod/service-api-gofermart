7a8712075a78ac40bc32a4e35d735185fc7fa896
Saveliy Yudin <yudinsv@agata-hub.ru>
2022-11-21 12:27:35 +0300

    fix: изменить quit на буферизированный канал

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -24,7 +24,7 @@ func NewServer(r *gin.Engine, cfg models.Config) {
 			log.Fatalf("ошибка: %s\n\n", err)
 		}
 	}()
-	quit := make(chan os.Signal)
+	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
